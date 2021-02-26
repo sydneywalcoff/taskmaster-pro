1eadb691e8395c73ac64304b86cd28182d667769
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-25 19:33:15 -0800

    object optimization of the update function --5.3.5 (near bottom)

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,6 +122,44 @@ $(".list-group").on("blur", "input[type='text']", function(){
   $(this).replaceWith(taskSpan);
 });
 
+// makes the lists sortable across lists and within themselves
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this).find("p").text().trim();
+
+      var date = $(this).find("span").text().trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+
+    });
+
+    console.log(tempArr);
+  }
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
