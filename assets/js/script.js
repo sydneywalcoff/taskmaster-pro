b5c822d133c0884f1752b2c89ce3df854ca3710d
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-02-25 21:14:29 -0800

    trash draggable now

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,7 +156,30 @@ $(".card .list-group").sortable({
 
     });
 
-    console.log(tempArr);
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+// making trash droppable
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log('out');
   }
 });
 
